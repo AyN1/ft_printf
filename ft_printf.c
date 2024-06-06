c1f94155622a2544a3fa53bd6244c3c8fc559fdd
atbicer <atbicer@student.s19.be>
2024-06-06 12:26:15 +0200

    progress made

--- ft_printf.c
+++ ft_printf.c
@@ -1,13 +1,113 @@
-#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 12:25:35 by atbicer           #+#    #+#             */
+/*   Updated: 2024/06/06 12:25:35 by atbicer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	print_char(int c)
 {
-	
-	return ;
+	return (write(1, &c, 1));
+}
+
+int	print_str(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str != 0)
+	{
+		print_char((int)*str);
+		++count;
+		++str;
+	}
+	return (count);
+}
+
+int	print_digit(long n, int base)
+{
+	int		count;
+	char	*symbols;
+
+	symbols = "0123456789abcdef";
+	count = 0;
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return (print_digit(-n, base) + 1);
+	}
+	else if (n < base)
+		return (print_char(symbols[n]));
+	else
+	{
+		count = print_digit(n / base, base);
+		return (count + print_digit(n % base, base));
+	}
+}
+
+int	print_format(char specifier, va_list ap)
+{
+	int	count;
+
+	count = 0;
+	if (specifier == 'c')
+		count = print_char(va_arg(ap, int));
+	else if (specifier == 's')
+		count += print_str(va_arg(ap, char *));
+	else if (specifier == 'd')
+		count += print_digit((long)va_arg(ap, int), 10);
+	else if (specifier == 'x')
+		count += print_digit((long)va_arg(ap, unsigned int), 16);
+	else
+		count += write(1, &specifier, 1);
+	return (count);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		count;
+
+	va_start(ap, format);
+	count = 0;
+	while (*format != 0)
+	{
+		if (*format == '%')
+			count += print_format(*(++format), ap);
+		else
+			count += write(1, format, 1);
+		++format;
+	}
+	va_end(ap);
+	return (count);
 }
 
 int	main(void)
 {
-	ft_putstr_fd("Hello", 1);
+	int	count;
+	int	test;
+
+	test = INT32_MIN;
+	ft_printf("Hello %s, today is %d, in hex %x, char->%c\n");
+	// printf("Hello %s, today is %d, in hex %x, char->%c\n");
+	// ft_printf("test\n");
+	/*	count = ft_printf("Hello %s\n", "John");
+		ft_printf("count(%d)\n", count);
+		count = ft_printf("[%d]\n", test);
+		ft_printf("The chars written are [%d]\n", count);
+		// real
+		count = printf("[%d]\n", test);
+		printf("The chars written are [%d]\n", count);
+	*/
 	return (0);
 }
