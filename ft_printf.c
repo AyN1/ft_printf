a83ab7e15f025ffa092d668986ae97c401f18308
atbicer <atbicer@student.s19.be>
2024-06-26 13:37:53 +0200

    unsigned int specifier done

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 12:25:35 by atbicer           #+#    #+#             */
-/*   Updated: 2024/06/26 13:19:37 by atbicer          ###   ########.fr       */
+/*   Updated: 2024/06/26 13:36:55 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int	print_format(char specifier, va_list ap)
 		count += ft_print_ptr(va_arg(ap, unsigned long long));
 	else if (specifier == 'd' || specifier == 'i')
 		count += ft_print_digit((long)va_arg(ap, int), 10, specifier);
+	else if (specifier == 'u')
+		count += ft_print_digit((long)va_arg(ap, unsigned int), 10, specifier);
 	else if (specifier == 'x' || specifier == 'X')
 		count += ft_print_digit((long)va_arg(ap, unsigned int), 16, specifier);
 	else
--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 13:14:30 by atbicer           #+#    #+#             */
-/*   Updated: 2024/06/26 13:13:38 by atbicer          ###   ########.fr       */
+/*   Updated: 2024/06/26 13:33:42 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,9 +32,22 @@ int	main(void)
 	ft_printf("The chars written are [%d]\n", count);
 	ft_printf("hex: [%X]\n", 100023);
 	ft_printf("%%\n");
-	//
-	count = ft_printf("Addr: [%p]\n", (void *)-14523); // p
+	// Address
+	count = ft_printf("Addr: [%p]\n", (void *)-14523);
 	ft_printf("Addr: count(%d)\n", count);
+	// Unsigned int
+	ft_printf("\nUINT tests: \n");
+	ft_printf("[%u]\n", 0);
+	ft_printf("[%u]\n", -10);
+	ft_printf("[%u]\n", -200000);
+	ft_printf("[%u]\n", -6000023);
+	ft_printf("[%u]\n", 10000);
+	ft_printf("[%u]\n", 100023);
+	ft_printf("[%u]\n", INT_MAX);
+	ft_printf("[%u]\n", INT_MIN);
+	ft_printf("[%u]\n", UINT_MAX);
+	ft_printf("dgs%uxx\n", 10);
+	ft_printf("%u%uu%u\n", 1, 2, -3);
 	//
 	printf("\n\n[Real below]\n\n"); // real
 	//
@@ -43,9 +57,22 @@ int	main(void)
 	printf("The chars written are [%d]\n", count);
 	printf("hex: [%X]\n", 100023);
 	printf("%%\n");
-	//
+	// Address
 	count = printf("Addr: [%p]\n", (void *)-14523); // p
 	printf("Addr: count(%d)\n", count);
+	// Unsigned int
+	printf("\nUINT tests: \n");
+	printf("[%u]\n", 0);
+	printf("[%u]\n", -10);
+	printf("[%u]\n", -200000);
+	printf("[%u]\n", -6000023);
+	printf("[%u]\n", 10000);
+	printf("[%u]\n", 100023);
+	printf("[%u]\n", INT_MAX);
+	printf("[%u]\n", INT_MIN);
+	printf("[%u]\n", UINT_MAX);
+	printf("dgs%uxx\n", 10);
+	printf("%u%uu%u\n", 1, 2, -3);
 	//
 	// LIBFT test
 	return (0);
