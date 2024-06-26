47e8c0925d99997cb0ab9411d3a1db636b600a6e
Ayn <atbicer@student.s19.be>
2024-06-26 20:49:58 +0200

    ft_printf.h removing unused headers and adding ft_printf protype

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 13:38:54 by atbicer           #+#    #+#             */
-/*   Updated: 2024/06/26 13:39:00 by atbicer          ###   ########.fr       */
+/*   Updated: 2024/06/26 20:47:01 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <stdio.h>
-# include <stdlib.h>
 # include <unistd.h>
 
+int	ft_printf(const char *format, ...);
 int	ft_print_digit(long n, int base, char specifier);
 int	ft_print_char(int c);
 int	ft_print_str(char *str);
